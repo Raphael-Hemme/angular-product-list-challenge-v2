3b53aac851d2f638ec84c703e247fc92fbd94d65
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-09-04 10:14:45 +0200

    setup: Rearrange project structure and generate ApiService

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { ProductListPageComponent } from './pages/product-list-page/product-list-page.component';
-import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { ProductListPageComponent } from './products/pages/product-list-page/product-list-page.component';
+import { NotFoundPageComponent } from './products/pages/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
   {
