111147954e048c6391a2c88548315839af3b05ba
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-09-04 08:50:01 +0200

    setup: use experimental zoneless change detection and ChangeDetectionStrategy.OnPush to use with signals mainly

--- src/app/pages/product-details-page/product-details-page.component.ts
+++ src/app/pages/product-details-page/product-details-page.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-details-page',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   templateUrl: './product-details-page.component.html',
-  styleUrl: './product-details-page.component.scss'
+  styleUrl: './product-details-page.component.scss',
 })
-export class ProductDetailsPageComponent {
-
-}
+export class ProductDetailsPageComponent {}
--- src/app/pages/product-list-page/product-list-page.component.ts
+++ src/app/pages/product-list-page/product-list-page.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-list-page',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   templateUrl: './product-list-page.component.html',
-  styleUrl: './product-list-page.component.scss'
+  styleUrl: './product-list-page.component.scss',
 })
-export class ProductListPageComponent {
-
-}
+export class ProductListPageComponent {}
