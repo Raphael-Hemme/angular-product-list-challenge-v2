cd3aaa14b53c4423bcd86a5e0c85decebcee4e4c
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-09-04 12:43:15 +0200

    setup: Add routes with redirects and params for products/:page and product/:id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,32 @@
 import { Routes } from '@angular/router';
 import { ProductListPageComponent } from './products/pages/product-list-page/product-list-page.component';
 import { NotFoundPageComponent } from './products/pages/not-found-page/not-found-page.component';
+import { ProductDetailsPageComponent } from './products/pages/product-details-page/product-details-page.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'products/:page',
+    title: 'Products',
     component: ProductListPageComponent,
   },
   {
+    path: 'product/:id',
+    title: 'Product Details',
+    component: ProductDetailsPageComponent,
+  },
+  {
+    path: 'products',
+    redirectTo: 'products/1',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    redirectTo: '/products/1',
+    pathMatch: 'full',
+  },
+  {
     path: '**',
+    title: 'Not Found',
     component: NotFoundPageComponent,
   },
 ];
