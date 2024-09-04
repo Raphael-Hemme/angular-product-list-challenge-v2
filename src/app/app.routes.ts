0d120a176d35320905436b7899d3426c155ecc46
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-09-04 09:49:27 +0200

    feat: Add a minimal 404 page and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ProductListPageComponent } from './pages/product-list-page/product-list-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ProductListPageComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent,
   },
 ];
