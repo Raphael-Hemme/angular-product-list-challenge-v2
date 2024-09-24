2bd4e029fe467a0e2ffa84d951362692fa31850f
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-09-24 20:57:30 +0200

    refactor: Small refactoring in ProductDetailsComponent to remove magic number code smell and add comments

--- src/app/products/components/product-details/product-details.component.ts
+++ src/app/products/components/product-details/product-details.component.ts
@@ -10,6 +10,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { NgOptimizedImage } from '@angular/common';
 import { SkeletonComponent } from '../../../core/components/skeleton/skeleton.component';
 
+const defaultEdgeLength = 400;
+
 @Component({
   selector: 'app-product-details',
   standalone: true,
@@ -26,7 +28,14 @@ export class ProductDetailsComponent {
   public skeletonWidth = `${this.imageEdgeLength}px`;
   public skeletonHeight = `${this.imageEdgeLength}px`;
 
+  /**
+   * Calculates the dimensions for an image based on the current window size to make sure it fits both on mobile and desktop.
+   *
+   * @returns {number} The smaller of the window's width or height, multiplied by 0.8.
+   * If the window dimensions are not available or invalid, returns a default edge length.
+   */
   private getImageDimensions(): number {
+    // Get the window dimensions
     const windowDimensions = [
       Math.round(window.innerWidth),
       Math.round(window.innerHeight)
@@ -38,9 +47,10 @@ export class ProductDetailsComponent {
       windowDimensions.some((dim) => dim <= 0) ||
       windowDimensions.some((dim) => isNaN(dim))
     ) {
-      return 400;
+      return defaultEdgeLength;
     }
 
+    // Return the smaller of the two dimensions, multiplied by 0.8 to make sure the image fits both on mobile and desktop
     return Math.round(Math.min(...windowDimensions) * 0.8);
   }
 }
