c726eec1c582c98a9344f19d1736beff97a4085c
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-10-08 17:06:03 +0200

    refactor: Remove Angular Material dependency from NotFoundPageComponent and adjust styling slightly

--- src/app/products/pages/not-found-page/not-found-page.component.ts
+++ src/app/products/pages/not-found-page/not-found-page.component.ts
@@ -1,13 +1,12 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { NavigationService } from '../../../core/services/navigation/navigation.service';
 import { ErrorUiComponent } from '../../../core/components/error-ui/error-ui.component';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-not-found-page',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [ErrorUiComponent, MatButtonModule],
+  imports: [ErrorUiComponent],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.scss'
 })
