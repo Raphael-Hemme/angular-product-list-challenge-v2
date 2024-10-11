8925ad2c9a5667cf0f630d7fa455475fca0256df
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-10-11 16:32:16 +0200

    cosmetic: More work on transition to tailwind styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ['./src/**/*.{html,ts}'];
 export const theme = {
-  extend: {}
+  extend: {
+    fontFamily: {
+      console: ['Inconsolata', 'monospace'],
+      script: ['Playwrite', 'cursive']
+    }
+  }
 };
 export const plugins = [];
