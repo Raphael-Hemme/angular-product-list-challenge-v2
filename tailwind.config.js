d1f4b8c563985d99a33a20decab72af1d5a8ddbb
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2024-10-18 12:16:38 +0200

    refactor: remove variables.scss and containing folder and all references

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ export const theme = {
     fontFamily: {
       console: ['Inconsolata', 'monospace'],
       script: ['Playwrite', 'cursive']
+    },
+    colors: {
+      primary: 'rgba(104, 192, 210, 1)',
+      secondary: 'rgba(169, 196, 120, 1)'
     }
   }
 };
